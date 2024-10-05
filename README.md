458014445a4acab0fecd731428fc060273e25743
lijinhong11 <tygfhk@outlook.com>
2024-10-05 11:33:21 +0800

    improve update checker

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# NMSAPI
+# NMS-API
+An api for using NMS utils from bukkit plugins.
 
-An api for build a bridge from NMS(`net.minecraft.server`) to bukkit plugins.
-
-Javadoc: https://javadoc.io/doc/io.github.linsminecraftstudio/nmsapi/latest/ (not available yet)
+Javadoc: https://javadoc.io/doc/io.github.linsminecraftstudio/nms-api/latest/index.html
 
 ## Requirements
 
 - Java 17 or higher
-- Minecraft server 1.20.4 or higher
-- Paper(or its fork) server **(We won't support Spigot servers, because that's terrible)**
+- Minecraft version 1.20.4+
+- Paper(or its fork) server
 
-## Installation
+## Use
 
 ### Maven
 
@@ -19,6 +18,7 @@ Javadoc: https://javadoc.io/doc/io.github.linsminecraftstudio/nmsapi/latest/ (no
     <groupId>io.github.linsminecraftstudio</groupId>
     <artifactId>nmsapi</artifactId>
     <version>THE_VERSION_YOU_WANT_TO_USE</version>
+    <scope>provided</scope>
 </dependency>
 ```
 
--- artifact/src/main/java/io/github/linsminecraftstudio/nmsapi/NMSAPIPlugin.java
+++ artifact/src/main/java/io/github/linsminecraftstudio/nmsapi/NMSAPIPlugin.java
@@ -6,14 +6,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
 public class NMSAPIPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("NMSAPI is enabled!");
-        checkUpdate();
+        getServer().getScheduler().runTaskTimerAsynchronously(this, this::checkUpdate, 0, 20 * 60 * 60 * 24); // Check for updates every 24 hours
     }
 
     @Override
@@ -27,21 +26,40 @@ public class NMSAPIPlugin extends JavaPlugin {
                 .uri(URI.create("https://hangar.papermc.io/api/v1/projects/nms-api/latestrelease"))
                 .GET()
                 .build();
-        CompletableFuture<HttpResponse<String>> ver = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
         try {
-            ver.whenCompleteAsync((h, e) -> {
-               if (e != null) {
-                   getLogger().log(Level.SEVERE, "Failed to check NMSAPI updates", e);
-               } else {
-                   String latest = h.body();
-                   if (!this.getPluginMeta().getVersion().equalsIgnoreCase(latest)) {
-                       getLogger().warning("A new version of NMSAPI is available! You are running version " + this.getPluginMeta().getVersion() + " and the latest version is " + latest + ".");
-                       getLogger().warning("Download it at https://hangar.papermc.io/lijinhong11/NMS-API/versions");
-                   } else {
-                       getLogger().info("NMSAPI is up to date!");
-                   }
-               }
-            }).get();
+            String current = this.getPluginMeta().getVersion();
+            HttpResponse<String> ver = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String latest = ver.body();
+            if (!current.equalsIgnoreCase(latest)) {
+                getLogger().warning("A new version of NMSAPI is available! You are running version " + this.getPluginMeta().getVersion() + " and the latest version is " + latest + ".");
+                getLogger().warning("Download it at https://hangar.papermc.io/lijinhong11/NMS-API/versions");
+            } else {
+                if (current.contains("SNAPSHOT")) {
+                    HttpRequest request2 = HttpRequest.newBuilder()
+                            .uri(URI.create("https://hangar.papermc.io/api/v1/projects/nms-api/latest?channel=Snapshot"))
+                            .GET()
+                            .build();
+                    HttpResponse<String> ver2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+                    String latest2 = ver2.body();
+                    if (!latest2.equalsIgnoreCase(latest)) {
+                        getLogger().warning("""
+                                NMSAPI update available! You are running a snapshot version of NMSAPI.
+                                The latest snapshot version is %s.
+                                The latest release version is %s.
+                                
+                                When you are ready to update, please switch to the release version.
+                                DON'T use the snapshot version unless you are willing to risk bugs or instability.
+                                
+                                Download it at https://hangar.papermc.io/lijinhong11/NMS-API/versions
+                                """);
+                    } else {
+                        getLogger().warning("You are running a snapshot version of NMSAPI. Proceed with caution.");
+                        getLogger().info("NMSAPI is up to date!");
+                    }
+                } else {
+                    getLogger().info("NMSAPI is up to date!");
+                }
+            }
         } catch (Exception e) {
             getLogger().log(Level.SEVERE, "Failed to check NMSAPI updates", e);
         }
